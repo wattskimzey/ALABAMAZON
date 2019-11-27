a6c1378b73b24f441478d3ccd9f3aa25595cc527
mrtwistyneck <wattskimzey@gmail.com>
2019-11-27 14:36:27 -0600

    plantin' seeds

---
+++ alabamazon-seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Clam Wax", "Clam Accessories", 9.99, 50);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("XXL Nose Trimmers", "Big Things", 6.99, 47);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Hot Dog Buns", "Adult Entertainment", 17.99, 58);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Wolf Nipple Chips", "Organic Snacks", 14.99, 44);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Eau de Parfum - Pepe", "Animal Bait", 57.99, 114);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Elbow Grease", "Arm Things", 29.95, 78);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Ear Piercer", "Glamor", 4.99, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Toejam Display Case", "Home Decor", 168.00, 37);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Beginner's Guide To Being A TA", "Crazy Stuff", 24.99, 17);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+Values ("Evil Clown Repellent", "Circus Safety", 69.99, 88);
