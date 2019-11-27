50ffb896562688e8475042e5a41d92f81fae3e0a
mrtwistyneck <wattskimzey@gmail.com>
2019-11-27 14:24:16 -0600

    yee haw on the JS

--- alabamzonCustomer.js
+++ alabamzonCustomer.js
@@ -12,3 +12,83 @@ var connection = mysql.createConnection({
     password: "root",
     database: "alabamazon"
 });
+connection.connect(function (err) {
+    if (err) throw err;
+
+    displayProducts();
+    setTimeout(shop, 3000);
+});
+
+function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        console.log("----------------------------");
+        console.log("ID | Product | Price");
+        console.log("----------------------------");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price);
+            console.log("----------------------------");
+        }
+    })
+};
+
+function shop() {
+    connection.query("SELECT * FROM products", function (err, res) {
+
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "list",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].item_id.toString());
+                        }
+                        return choiceArray;
+                    },
+                    message: "WHAT DO YOU WANNA BUY TATERHEAD?"
+                },
+                {
+                    name: "quantity",
+                    type: "list",
+                    choices: ["1", "2", "3", "4", "5"],
+                    message: "HOW MANY DO YA WANT?"
+                }
+            ])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].item_id === parseInt(answer.choice)) {
+                        chosenItem = res[i];
+                    }
+                }
+
+                var totalPaid = chosenItem.price * answer.quantity;
+
+                if(chosenItem.stock_quantity >= parseInt(answer.quantity)){
+                    connection.query(
+                        "Update products Set ? Where ?",
+                        [
+                            {
+                                stock_quantity: chosenItem.stock_quantity - answer.quantity
+                            },
+                            {
+                                item_id: chosenItem.item_id
+                            }
+                        ],
+                        function(error){
+                            if (error) throw err;
+                            console.log("GLORY BE YOU BOUGHT SOME STUFF! IT'S GONNA COST YA $" + totalPaid);
+                            setTimeout(displayProducts, 3000);
+                            setTimeout(shop, 5000);
+                        }
+                    )
+                }
+                else{
+                    console.log("LUKE HORKSTADER ALREADY BOUGHT ALL THESE.  TRY BUYIN' SOMETHIN' ELSE.");
+                    setTimeout(displayProducts, 3000);
+                    setTimeout(shop, 5000);
+                }
+            });
+    });
+};
